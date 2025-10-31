c73e41d58c8cce99e6ebf9a92d984d3528846ef3
Cristiano Nunes <cfgnunes@gmail.com>
2025-10-31 02:03:17 -0300

    fix: Ensure script compatibility with GNOME 48 and 49

--- nautilus-backspace.py
+++ nautilus-backspace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os, gi
-gi.require_version('Nautilus', '4.1')
 gi.require_version('Gtk', '4.0')
 from gi.repository import GObject, Nautilus, Gtk, Gio, GLib
 
@@ -9,7 +8,7 @@ class BackspaceBack(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
         super().__init__()
         pass
-    
+
     def get_file_items(self, *args):
         app = Gtk.Application.get_default()
         app.set_accels_for_action("slot.back", ["BackSpace"])
